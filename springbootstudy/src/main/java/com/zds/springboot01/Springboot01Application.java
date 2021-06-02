e02ed6c1f13fdf3f0c8ba103aecd84df3aad7e65
zhongds01 <zhongds01@163.com>
2021-06-02 22:05:33 +0800

    @Import注解使用

--- springbootstudy/src/main/java/com/zds/springboot01/Springboot01Application.java
+++ springbootstudy/src/main/java/com/zds/springboot01/Springboot01Application.java
@@ -1,5 +1,6 @@
 package com.zds.springboot01;
 
+import ch.qos.logback.classic.log4j.XMLLayout;
 import com.zds.springboot01.bean.User;
 import com.zds.springboot01.bean.UserEnableConfigurationProperties;
 import com.zds.springboot01.config.SpringConfig;
@@ -8,15 +9,19 @@ import com.zds.springboot01.service.TreeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
+import javax.xml.bind.DatatypeConverter;
 import java.util.Arrays;
 
 /**
  * ImportSource注解用于引入自定义的spring xml配置文件
+ * Import通过类名导入该类的实例，实例名为类的全类名
  */
 @SpringBootApplication
 @ImportResource(value = {"classpath:beans.xml"})
+@Import(value = {XMLLayout.class, DatatypeConverter.class})
 public class Springboot01Application {
 
     public static void main(String[] args) {
@@ -47,9 +52,17 @@ public class Springboot01Application {
         UserEnableConfigurationProperties userEnableConfigurationProperties = applicationContext.getBean("third.user-com.zds.springboot01.bean.UserEnableConfigurationProperties", UserEnableConfigurationProperties.class);
         System.out.println(userEnableConfigurationProperties);
 
-        // 验证@@ImportResource注解是否成功引入beans.xml，如果成功，可以获取userBaseXml实例 --> true
+        // 验证@ImportResource注解是否成功引入beans.xml，如果成功，可以获取userBaseXml实例 --> true
         System.out.println("是否成功注入userBaseXml: " + applicationContext.containsBean("userBaseXml"));
         String[] beanNamesForType1 = applicationContext.getBeanNamesForType(User.class);
+
+        // 验证@Import注解是否有效，获取XMLLayout和DatatypeConverter实例
+        // --> [javax.xml.bind.DatatypeConverter]
+        String[] datatypeConverterNames = applicationContext.getBeanNamesForType(DatatypeConverter.class);
+        System.out.println(Arrays.toString(datatypeConverterNames));
+        // --> [ch.qos.logback.classic.log4j.XMLLayout]
+        String[] xmlLayOutNames = applicationContext.getBeanNamesForType(XMLLayout.class);
+        System.out.println(Arrays.toString(xmlLayOutNames));
 
         for (String s : beanNamesForType1) {
             System.out.println(s);
